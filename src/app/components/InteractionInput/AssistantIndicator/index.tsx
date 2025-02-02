5b4ba5856be62302150f4385b267a6103b7f4bcc
Nicolás Frías <nicolas.friasrojas@gmail.com>
2025-02-02 12:45:48 -0300

    feat: :sparkles: Implemented assistant indicator

---
+++ src/app/components/InteractionInput/AssistantIndicator/index.tsx
@@ -0,0 +1,9 @@
+export function AssistantIndicator() {
+  return (
+    <div className="relative size-64 scale-75">
+      <div className="blob layer-1 animate" />
+      <div className="blob layer-2" />
+      <div className="blob layer-3 animate" />
+    </div>
+  );
+}
